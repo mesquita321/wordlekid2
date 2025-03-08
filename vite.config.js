2df7fb04ccfcb9dd32602b8e741a6e35befe07fe
João Mesquita <joao@MacBook-Air-de-Joao.local>
2025-03-08 14:29:23 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: ['main-gecko-pro.ngrok-free.app'],
-  },
-  base: "/wordlekid/",
+  base: "/wordlekid2/",
   plugins: [react()],
 })
